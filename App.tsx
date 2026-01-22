8ec244c2b7b12c85ed6fe51458947efa3cbb9b0a
AllaBeloshapka <allabeloshapka4@gmail.com>
2026-01-22 13:43:45 +0100

    node modules

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 
-import Homework_17 from "./Homeworks/hw_17/Homework_17"; 
-import Lesson_17 from "./Lessons/Lesson_17/Lesson_17"
+
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom"
 import Layout from "./Lessons/Lesson_18/Project_15/Layout"
 import Create_Employee from "./Lessons/Lesson_18/Project_15/Create_Employee/Create_Employee";
